f2feb620f5f89508105e7415e545efe4570be35f
Nilesh Wankhede <114302591+nileshwankhede03@users.noreply.github.com>
2025-07-16 00:50:38 +0530

    First commit

---
+++ program4.js
@@ -0,0 +1,14 @@
+
+function Addition(no1 , no2)
+{
+    let ans = 0
+
+    ans = no1 + no2
+
+    return ans
+}
+
+let value1 = 10
+let value2 = 11
+let iRet = Addition(value1,value2)
+console.log("Addition is : "+iRet);
---
+++ program5.js
@@ -0,0 +1,13 @@
+let A = 11
+let B = true
+let C = "Javascript"
+let D
+let E = null
+let F = 3948624284738348392473829n
+
+console.log(typeof(A));
+console.log(typeof(B));
+console.log(typeof(C));
+console.log(typeof(D));
+console.log(typeof(E));
+console.log(typeof(F));
