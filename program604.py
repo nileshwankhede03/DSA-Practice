c2ac4ca7e762caf0a5b979a286ab3ede582d5790
Nilesh Wankhede <114302591+nileshwankhede03@users.noreply.github.com>
2025-08-03 21:02:19 +0530

    Initial Commit

---
+++ program604.py
@@ -0,0 +1,13 @@
+no1 = 0
+no2 = 0
+ans = 0
+
+print("Enter First number : ")
+no1 = int(input())
+
+print("Enter Second number : ")
+no2 = int(input())
+
+ans = no1 + no2
+
+print(f"Addition of {no1} & {no2} is {ans} ")
---
+++ program605.py
@@ -0,0 +1,8 @@
+
+def Display():
+    print("Inside display")
+    print("Pune Maharashtra")
+
+Display()
+
+print("End of program")
